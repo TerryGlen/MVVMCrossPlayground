3d1b7a05ac6ef376c02f28145e659fab546f804f
terry <saispas99@gmail.com>
2021-03-03 17:27:24 -0600

    Initial Commit
    * Main.cs:
    * Setup.cs:
    * Info.plist:
    * Main.storyboard:
    * RootView.cs:
    * Tab3View.cs:
    * Tab2View.cs:
    * Tab1View.cs:
    * ModalView.cs:
    * ChildView.cs:
    * home.png:
    * Entitlements.plist:
    * Page3View.cs:
    * Page2View.cs:
    * Page1View.cs:
    * TabsRootView.cs:
    * ModalNavView.cs:
    * home@2x.png:
    * Playground.iOS.csproj:
    * home@3x.png:
    * settings.png:
    * PagesRootView.cs:
    * BinaryEdit.cs:
    * SplitRootView.cs:
    * LaunchScreen.storyboard:
    * NestedModalView.cs:
    * SecondChildView.cs:
    * SplitMasterView.cs:
    * SplitDetailView.cs:
    * settings@2x.png:
    * settings@3x.png:
    * Tab1View.designer.cs:
    * RootView.designer.cs:
    * Tab2View.designer.cs:
    * Tab3View.designer.cs:
    * CustomBindingView.cs:
    * ChildView.designer.cs:
    * SplitDetailNavView.cs:
    * Page1View.designer.cs:
    * Page2View.designer.cs:
    * Page3View.designer.cs:
    * ModalView.designer.cs:
    * ic_tabbar_menu.png:
    * Contents.json:
    * TabsRootView.designer.cs:
    * OverrideAttributeView.cs:
    * ModalNavView.designer.cs:
    * ic_tabbar_menu@3x.png:
    * PagesRootView.designer.cs:
    * SplitRootView.designer.cs:
    * ic_tabbar_menu@2x.png:
    * SecondChildView.designer.cs:
    * NestedModalView.designer.cs:
    * SplitDetailView.designer.cs:
    * SplitMasterView.designer.cs:
    * CustomBindingView.designer.cs:
    * BinaryEditTargetBinding.cs:
    * SplitDetailNavView.designer.cs:
    * OverrideAttributeView.designer.cs:
    * Contents.json:
    * Contents.json:
    * baseline_keyboard_arrow_down_black_18pt_2x.png:
    * baseline_keyboard_arrow_down_black_18pt_1x.png:
    * baseline_keyboard_arrow_down_black_18pt_3x.png:

---
+++ Views/NestedModalView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MvvmCross.Platforms.Ios.Views;
+using UIKit;
+using Playground.Core.ViewModels;
+using MvvmCross.Platforms.Ios.Presenters.Attributes;
+
+namespace Playground.iOS.Views
+{
+    [MvxFromStoryboard("Main")]
+    [MvxModalPresentation(WrapInNavigationController = true)]
+    public partial class NestedModalView : MvxViewController<NestedModalViewModel>
+    {
+        public NestedModalView(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            View.BackgroundColor = UIColor.Orange;
+
+            var set = CreateBindingSet();
+            set.Bind(btnTabs).To(vm => vm.ShowTabsCommand);
+            set.Bind(btnClose).To(vm => vm.CloseCommand);
+            set.Apply();
+        }
+    }
+}
